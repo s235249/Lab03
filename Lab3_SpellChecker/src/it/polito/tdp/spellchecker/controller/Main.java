159cdf79841827227e102d2a81cccf73758c50eb
tattommasi <47092404+tattommasi@users.noreply.github.com>
2019-04-15 11:27:21 +0200

    soluzione

--- Lab3_SpellChecker/src/it/polito/tdp/spellchecker/controller/Main.java
+++ Lab3_SpellChecker/src/it/polito/tdp/spellchecker/controller/Main.java
@@ -1,25 +1,34 @@
 package it.polito.tdp.spellchecker.controller;
 
+import it.polito.tdp.spellchecker.model.Dictionary;
 import javafx.application.Application;
-import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
-import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
 
 public class Main extends Application {
+	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("SpellChecker.fxml"));
-			Scene scene = new Scene(root,400,400);
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("SpellChecker.fxml"));
+			BorderPane root = (BorderPane) loader.load();
+
+			SpellCheckerController controller = loader.getController();
+			Dictionary model = new Dictionary();
+			controller.setModel(model);
+
+			Scene scene = new Scene(root);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
-		} catch(Exception e) {
+
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		launch(args);
 	}
